09f7b0390527c5f9a939ab362940a0c0d97d73c3
Jetro Bagasala <jethgen.26@gmail.com>
2024-03-04 14:16:16 +0800

    Update responsive rendering

--- academic_year.php
+++ academic_year.php
@@ -141,11 +141,25 @@ $(document).ready(function() {
       {
         "data": null,
         "render": function(data, type, row) {
-          return '<i class="fas fa-trash-alt delete-btn text-danger" title="Delete"></i>';
+          return '<i class="fas fa-trash-alt delete-btn text-danger"></i>';
         }
       }
     ],
-    "responsive": true,
+    "responsive": {
+        details: {
+          renderer: function(api, rowIdx, columns) {
+            var data = $.map(columns, function(col, i) {
+              return col.hidden ?
+                '<tr data-dt-row="' + col.rowIndex + '" data-dt-column="' + col.columnIndex + '">' +
+                '<td>' + col.title + ':' + '</td> ' +
+                '<td>' + col.data + '</td>' +
+                '</tr>' :
+                '';
+            }).join('');
+            return data ? $('<table/>').append(data) : false;
+          }
+        }
+      },
     "lengthChange": false, 
     "autoWidth": false,
     "dom": 'Bfrtip', 
