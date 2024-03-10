c87ad0fe2c3fe9ae3ff748f3c0585d1e59d15173
Jetro Bagasala <jethgen.26@gmail.com>
2024-03-10 17:46:52 +0800

    added title icon

--- registrar_dashboard.php
+++ registrar_dashboard.php
@@ -43,7 +43,8 @@ $result = mysqli_query($conn, $query);
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>ATMOS | Registrar</title>
+  <link rel="icon" type="image/x-icon" href="dist/img/icon.png">
+  <title>ATMOS | Dashboard</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
--- registrar_report.php
+++ registrar_report.php
@@ -42,7 +42,8 @@ $result = mysqli_query($conn, $query);
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>ATMOS | Registrar</title>
+  <link rel="icon" type="image/x-icon" href="dist/img/icon.png">
+  <title>ATMOS | Report</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
