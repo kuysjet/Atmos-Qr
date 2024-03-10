311dd21e77657c819aae1afecf8475967938ff2b
Cedrick Embestro <cedrick13wew@gmail.com>
2024-03-09 23:07:46 -0800

    Add Icon

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="dist/img/icon.png">
   <title>ATMOS</title>
 
   <script>
