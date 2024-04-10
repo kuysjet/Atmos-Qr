973ccfb7f0779c27954f1a94221a694f4a074bc2
Jetro Bagasala <jethgen.26@gmail.com>
2024-04-10 13:56:56 +0800

    add tooltip, adjust time size

--- event.php
+++ event.php
@@ -38,7 +38,7 @@ if ($result) {
 <?php include 'includes/header.php'; ?>
 
   <!-- DataTables -->
-  <link rel="stylesheet" href="plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <!-- <link rel="stylesheet" href="plugins/datatables-bs4/css/dataTables.bootstrap4.min.css"> -->
   <link rel="stylesheet" href="plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
   <link rel="stylesheet" href="plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
   <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
@@ -68,10 +68,10 @@ if ($result) {
         <div class="col-sm-6">
           <div class="row">
             <div class="col-sm-12 text-sm-right">
-              <div class="mr-2 small"><b>Philippine Standard Time</b></div>
+              <div class="mr-3 small"><b>Philippine Standard Time</b></div>
             </div>
             <div class="col-sm-12 text-sm-right">
-              <div id="philippine-date-time" class="small"></div>
+              <div id="philippine-date-time" style="font-size: 15px;"></div>
             </div>
           </div>
         </div><!-- /.col -->
@@ -94,7 +94,7 @@ if ($result) {
               </div>
               <!-- /.card-header -->
               <div class="card-body">
-                <table id="eventsTable" class="display table table-bordered" style="display: none;">
+                <table id="eventsTable" class="display table table-bordered nowrap" style="display: none;">
                   <thead>
                   <tr>
                     <th>No.</th>
@@ -120,12 +120,11 @@ if ($result) {
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
-
-  <?php include 'includes/footer.php';?>
-  
 </div>
 <!-- ./wrapper -->
 
+<?php include 'includes/footer.php';?>
+  
 <!-- REQUIRED SCRIPTS -->
 
   <!-- jQuery -->
@@ -138,11 +137,12 @@ if ($result) {
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <!-- DataTables  & Plugins -->
   <script src="plugins/datatables/jquery.dataTables.min.js"></script>
-  <script src="plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+  <!-- <script src="plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script> -->
   <script src="plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
   <script src="plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
   <script src="plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
   <script src="plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+  <script src="https://cdn.datatables.net/1.13.7/js/dataTables.jqueryui.min.js"></script>
   <script src="plugins/jszip/jszip.min.js"></script>
   <script src="plugins/pdfmake/pdfmake.min.js"></script>
   <script src="plugins/pdfmake/vfs_fonts.js"></script>
@@ -199,8 +199,8 @@ if ($result) {
           "data": null,
           "render": function(data, type, row) {
             return '<div class="btn-group">' +
-                    '<button type="button" class="btn btn-primary btn-sm edit-btn mr-1"><i class="fas fa-edit"></i></button>' +
-                    '<button type="button" class="btn btn-danger btn-sm delete-btn"><i class="fas fa-trash-alt"></i></button>' +
+                    '<button type="button" class="btn btn-primary btn-sm edit-btn"><i class="fas fa-edit"></i></button>' +
+                    '<button type="button" class="btn btn-danger btn-sm delete-btn mx-1"><i class="fas fa-trash-alt"></i></button>' +
                   '</div>';
           }
         }
@@ -226,7 +226,8 @@ if ($result) {
       "buttons": [
         {
           extend: 'colvis',
-          text: '<i class="fas fa-eye"></i>',
+          text: '<i class="fas fa-columns"></i>',
+          titleAttr: 'Column Visibility', // Tooltip for the button
           className: 'btn-sm btn-light border',
         }
       ]
