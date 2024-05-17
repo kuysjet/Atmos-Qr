c8f6924f65fc099a7b184a9df7727f9b842497c8
Jetro Bagasala <jethgen.26@gmail.com>
2024-05-17 21:12:18 +0800

    add component scroll

---
+++ includes/scroll-button.php
@@ -0,0 +1,8 @@
+  <!-- Scroll to top button -->
+  <button class="btn btn-primary scroll-to-top d-sm-none shadow-none" onclick="scrollToTop()" style="opacity: 0.3; border-radius: 50%;">
+    <i class="fas fa-arrow-up"></i>
+  </button>
+  <!-- Scroll to bottom button -->
+  <button class="btn btn-primary scroll-to-bottom d-sm-none shadow-none" onclick="scrollToBottom()" style="opacity: 0.3; border-radius: 50%;">
+    <i class="fas fa-arrow-down"></i>
+  </button>
