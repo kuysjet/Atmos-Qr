91fcd8c27390fe2958934203ebd769c531bc382b
Jetro Bagasala <jethgen.26@gmail.com>
2024-05-17 21:16:46 +0800

    remove shadow scroll

--- scanner.php
+++ scanner.php
@@ -111,11 +111,11 @@ if (isset($_GET['eventId'])) {
 </head>
 <body>
   <!-- Scroll to top button -->
-  <button class="btn btn-primary scroll-to-top d-sm-none" onclick="scrollToTop()" style="opacity: 0.3; border-radius: 50%;">
+  <button class="btn btn-primary scroll-to-top d-sm-none shadow-none" onclick="scrollToTop()" style="opacity: 0.3; border-radius: 50%;">
     <i class="fas fa-arrow-up"></i>
   </button>
   <!-- Scroll to bottom button -->
-  <button class="btn btn-primary scroll-to-bottom d-sm-none" onclick="scrollToBottom()" style="opacity: 0.3; border-radius: 50%;">
+  <button class="btn btn-primary scroll-to-bottom d-sm-none shadow-none" onclick="scrollToBottom()" style="opacity: 0.3; border-radius: 50%;">
     <i class="fas fa-arrow-down"></i>
   </button>
 
